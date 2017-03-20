9a6768f50c1a5853aff232899a2ac0a7e32ca42a
Idaho Frost <frostidaho@gmail.com>
2017-03-20 16:55:05 -0400

    Add test_message.py

---
+++ tests/test_message.py
@@ -0,0 +1,29 @@
+from collections import namedtuple
+import pytest
+from asyncipc.message import BaseMessage, message_types
+
+class Alpha(BaseMessage):
+    _fields = ['a', 'b']
+    _kwfields = {'x': 42}
+
+class Beta(Alpha):
+    _fields = ['c']
+    _kwfields = {'y': 37}
+
+
+Param = namedtuple('Param', 'input expected')
+
+@pytest.mark.parametrize(
+    'msg,d_expected',
+    (
+        Param(Alpha(1,2, x=3), {'a':1, 'b':2, 'x':3}),
+        Param(Alpha(1,2), {'a':1, 'b':2, 'x': Alpha._kwfields['x']}),
+        Param(Beta(3,1,2), {'a':1, 'b':2, 'c':3, 'x': Alpha._kwfields['x'], 'y': Beta._kwfields['y']}),
+        Param(Beta('c','a','b', x='x',y='y'), {x:x for x in 'cabxy'}),
+    ),
+)
+def test_basic_msg(msg, d_expected):
+    for k,v in d_expected.items():
+        assert getattr(msg, k) == v
+    assert msg.as_dict == d_expected
+

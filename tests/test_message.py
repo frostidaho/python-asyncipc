af5b3b53bcb46e7bc6a0eac3340be22f11719256
Idaho Frost <frostidaho@gmail.com>
2017-03-20 17:02:48 -0400

    Add a few tests to test_message

--- tests/test_message.py
+++ tests/test_message.py
@@ -27,3 +27,16 @@ def test_basic_msg(msg, d_expected):
         assert getattr(msg, k) == v
     assert msg.as_dict == d_expected
 
+def test_sig():
+    sig = Alpha.__signature__
+    assert list(sig.parameters) == ['a', 'b', 'x']
+
+def test_sig_inheritance():
+    sig = Beta.__signature__
+    assert list(sig.parameters) == ['c', 'a', 'b', 'y', 'x']
+
+def test_repr():
+    a = Alpha(98, 99, x=97)
+    assert repr(a) == 'Alpha(a=98, b=99, x=97)'
+    
+    

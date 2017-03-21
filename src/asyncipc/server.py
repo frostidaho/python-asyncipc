7fa6cc3aa58b994ce242d1331f43b2a87a1adafe
Idaho Frost <frostidaho@gmail.com>
2017-03-20 22:21:42 -0400

    Update server.py

--- src/asyncipc/server.py
+++ src/asyncipc/server.py
@@ -4,37 +4,63 @@
 import asyncio
 from os import path as _path
 
-# from common import RUNTIME_DIR, serializers, HeaderFormat, MsgTuple
 from . import _utils
 from .serializer import Serialize
-
 
 class Server:
     def __init__(self, socket_name, message_types):
         self.socket_path = _path.join(_utils.RUNTIME_DIR, socket_name)
         self.logr = _utils.get_logger()
         self.serial = Serialize(**message_types)
+        self.messages = asyncio.Queue(30)
+        d_observer = {}
+        for key in message_types:
+            d_observer[key] = []
+        self.observers = d_observer
 
-    def __call__(self):
-        return asyncio.start_unix_server(self.router, path=self.socket_path)
+    def register(self, msgtype, fn):
+        d = self.observers
+        if isinstance(msgtype, str):
+            d[msgtype].append(fn)
+        else:
+            d[msgtype.__name__].append(fn)
+
+    def __call__(self, loop):
+        coro = asyncio.start_unix_server(self.listener, path=self.socket_path)
+        loop.run_until_complete(coro)
+        loop.create_task(self.queue_reader())
+
+    async def queue_reader(self):
+        while True:
+            msg = await self.messages.get()
+            msg_type = msg.__class__.__name__
+            observers = self.observers[msg_type]
+            if observers:
+                for fn in observers:
+                    fn(msg)
+            else:
+                self.logr.debug(f'{msg_type} has no observers')
 
     async def get_header(self, reader):
         data = await reader.read(self.serial.header_length)
         return self.serial.load(data)
 
-    async def router(self, reader, writer):
+    async def listener(self, reader, writer):
         logr = self.logr
-        logr.debug(f"In router!")
+        logr.debug(f"In listener!")
         header = await self.get_header(reader)
         value = await reader.read(header.data_length)
         msg = header.data_loader(value)
-        self.msg = msg
         logr.debug(f"Received {msg!r}")
-        writer.write(b'got it')
-        try:
-            await writer.drain()
-            logr.debug("Closing the client socket")
-            writer.close()
-        except ConnectionResetError:
-            pass
-        return msg
+        await self.messages.put(msg)
+        # self.msg = msg
+
+        # writer.close()
+        # writer.write(b'got it')
+        # try:
+        #     await writer.drain()
+        #     logr.debug("Closing the client socket")
+        #     writer.close()
+        # except ConnectionResetError:
+        #     pass
+        return
--- tests/test_server.py
+++ tests/test_server.py
@@ -10,14 +10,21 @@ logr = logging.getLogger()
 logr.addHandler(logging.StreamHandler())
 logr.setLevel(logging.DEBUG)
 
+class Recorder:
+    def __init__(self):
+        self.msg = None
+
+    def __call__(self, msg):
+        logr.debug(f'Got message {msg!r}')
+        self.msg = msg
+
 def test_send_msg(send_msg, sockpath, alpha):
     serv = Server(sockpath, message_types)
+    fn = Recorder()
+    serv.register(alpha.__class__, fn)
     loop = asyncio.get_event_loop()
-    coro = serv()
-    # fut = asyncio.ensure_future(coro)
-    loop.run_until_complete(coro)
+    serv(loop)
     loop.call_later(2.0, send_msg(alpha))
-    # loop.stop()
     loop.call_later(4.0, loop.stop)
     loop.run_forever()
-    assert serv.msg.as_dict == alpha.as_dict
+    assert fn.msg.as_dict == alpha.as_dict

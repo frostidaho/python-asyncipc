9eda9994241bfd4bf509d107fa5634a606bc96f9
Idaho Frost <frostidaho@gmail.com>
2017-03-24 21:53:39 -0400

    Delete message.py

--- tests/fixtures.py
+++ tests/fixtures.py
@@ -1,26 +1,6 @@
 import pytest
 
 from asyncipc.commands import RUNTIME_DIR
-from asyncipc.message import BaseMessage, message_types
-
-
-class Alpha(BaseMessage):
-    _fields = ['a', 'b']
-    _kwfields = {'x': 42}
-
-class Beta(Alpha):
-    _fields = ['c']
-    _kwfields = {'y': 37}
-
-
-@pytest.fixture(params=(Alpha(1,2),
-                        Beta(3,1,2,x=4,y=5),))
-def msg_obj(request):
-    yield request.param
-
-@pytest.fixture
-def alpha():
-    return Alpha(99,98)
 
 
 def new_socket(path):
